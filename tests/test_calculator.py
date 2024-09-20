f299b408203aee6c6fcd11b62e3c63f52dc5de1e
Savannah Chappus <savannahj1200@gmail.com>
2024-09-20 15:41:12 -0700

    removed system-dependent file imports

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,9 +1,6 @@
 import unittest
-import sys
 
-sys.path.insert(0, '../src/' )
-
-from calculator import add, subtract, divide
+from src.calculator import add, subtract, divide
 
 class TestCalculator( unittest.TestCase ):
     def test_add( self ):
