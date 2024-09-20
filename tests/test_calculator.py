f0481a4b4e8296e73c71b2020dec24341d42692a
Savannah Chappus <savannahj1200@gmail.com>
2024-09-20 15:36:52 -0700

    initializing repository with basic calculator and corresponding test file

---
+++ tests/test_calculator.py
@@ -0,0 +1,26 @@
+import unittest
+import sys
+
+sys.path.insert(0, '../src/' )
+
+from calculator import add, subtract, divide
+
+class TestCalculator( unittest.TestCase ):
+    def test_add( self ):
+        self.assertEqual( add( 2, 3 ), 5 )
+        self.assertEqual( add( -1, 1 ), 0 )
+        self.assertEqual( add( -1, -1 ), -2 )
+
+    def test_subtract( self ):
+        self.assertEqual( subtract( 4, 3 ), 1 )
+        self.assertEqual( subtract( 3, 4 ), -1 )
+        self.assertEqual( subtract( -1, -1 ), 0 )
+
+    def test_divide( self ):
+        self.assertMultiLineEqual( divide( 2, 0 ), "Error: divide by zero" )
+        self.assertEqual( divide( -1, 2 ), -0.5 )
+        self.assertEqual( divide( -10, -2 ), 5 )
+
+
+if __name__ == '__main__':
+    unittest.main()
